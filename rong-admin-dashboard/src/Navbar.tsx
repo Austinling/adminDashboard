58d631817f63b55b6e3f8a1d85eefec02a25f1e3
Austinling <94096647+Austinling@users.noreply.github.com>
2025-12-20 15:47:31 +0000

    Changed the way the navbar looked

--- rong-admin-dashboard/src/Navbar.tsx
+++ rong-admin-dashboard/src/Navbar.tsx
@@ -1,33 +1,41 @@
 import rongLogo from "./assets/images/rongbg.png";
-import cash from "./assets/images/cash.png";
-import user from "./assets/images/user.png";
-import loggedInUser from "./assets/images/loggedIn.png";
+import book from "./assets/images/book-keeping.png";
+import user from "./assets/images/twoUser.png";
+import logOut from "./assets/images/logout.png";
+import group from "./assets/images/group.png";
 
 export function Navbar() {
   return (
-    <div className="bg-[linear-gradient(90deg,rgba(242,128,128,1)_0%,rgba(247,230,230,1)_67%)] w-50 h-full border-r-2 border-black flex flex-col justify-around items-center gap-6 px-4">
+    <div className="bg-[linear-gradient(90deg,rgba(242,128,128,1)_0%,rgba(247,230,230,1)_67%)] w-50 h-full border-r-2 border-black flex flex-col items-center gap-6 px-4">
       <img src={rongLogo} alt="Logo" className="w-20 p-3 mt-10"></img>
 
-      <div className="flex flex-col justify-center items-center gap-5">
+      <div className="flex flex-1 flex-col justify-center gap-2">
         <div className="flex items-center">
-          <img src={cash} alt="Cash" className="w-15 p-3"></img>
+          <img src={user} alt="User" className="w-15 p-3"></img>
           <a href="" className="text-outline">
-            Payments
+            Users
           </a>
         </div>
 
         <div className="flex items-center">
-          <img src={user} alt="User" className="w-15 p-3"></img>
+          <img src={group} alt="User" className="w-15 p-3"></img>
           <a href="" className="text-outline">
-            Attendance
+            Teachers
+          </a>
+        </div>
+
+        <div className="flex items-center">
+          <img src={book} alt="Cash" className="w-15 p-3"></img>
+          <a href="" className="text-outline">
+            Payments
           </a>
         </div>
       </div>
 
       <div className="flex items-center">
-        <img src={loggedInUser} alt="User" className="w-15 p-3"></img>
-        <a href="" className="">
-          Austin
+        <img src={logOut} alt="User" className="w-13 p-3"></img>
+        <a href="" className="text-outline">
+          Logout
         </a>
       </div>
     </div>
