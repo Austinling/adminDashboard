08c1b883d4ded04bc14a2667522300a3df0972ec
Austinling <94096647+Austinling@users.noreply.github.com>
2025-12-20 16:32:44 +0000

    Added some styling to the data box

--- rong-admin-dashboard/src/main.tsx
+++ rong-admin-dashboard/src/main.tsx
@@ -8,8 +8,10 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <div className="flex h-screen bg-gray-50">
       <Navbar />
-      <div className="">
-        <FullDataBox />
+      <div className="flex-1 flex items-center justify-center">
+        <div className="bg-white h-150 w-300 border-0 shadow-lg rounded-xl ">
+          <FullDataBox />
+        </div>
       </div>
     </div>
   </StrictMode>
