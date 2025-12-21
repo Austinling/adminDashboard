20b3d078eed620d55745684eeb4b7a596bf60857
Austinling <94096647+Austinling@users.noreply.github.com>
2025-12-21 14:18:13 +0000

    Made filter have animations and made the overall design slightly nicer

--- rong-admin-dashboard/src/Filter.tsx
+++ rong-admin-dashboard/src/Filter.tsx
@@ -1,5 +1,6 @@
 import { gradeMap } from "./GradeMap.ts";
 import { useState } from "react";
+import filter from "./assets/images/filter.png";
 
 type FilterProps = {
   selectedGrades: string[];
@@ -20,29 +21,38 @@ export function Filter({ selectedGrades, setSelectedGrades }: FilterProps) {
   };
 
   return (
-    <div className="w-20 border-2">
-      <button className="w-20" onClick={() => setOpen(!open)}>
+    <div className="w-40 flex items-center justify-center border-2 mx-20 rounded-4xl">
+      <button
+        className="w-40 flex justify-center items-center"
+        onClick={() => setOpen(!open)}
+      >
         Filter Grades
+        <img src={filter} alt="filter" className="w-5 ml-4"></img>
       </button>
 
-      {open && (
-        <div className="absolute w-50 h-50">
-          {Object.entries(gradeMap).map(([key, value]) => {
-            const checked = selectedGrades.includes(key);
+      <div
+        className={`absolute w-50 h-100 bg-gray-600 z-15 top-30 flex flex-col items-center justify-center transform transition-all duration-300 ease-out
+      ${
+        open
+          ? "opacity-100 translate-y-0 scale-100"
+          : "opacity-0 -translate-y-3 scale-95"
+      }`}
+      >
+        {Object.entries(gradeMap).map(([key, value]) => {
+          const checked = selectedGrades.includes(key);
 
-            return (
-              <label key={key}>
-                <input
-                  type="checkbox"
-                  checked={checked}
-                  onChange={() => filterGrades(key)}
-                />
-                <span>{value}</span>
-              </label>
-            );
-          })}
-        </div>
-      )}
+          return (
+            <label key={key}>
+              <input
+                type="checkbox"
+                checked={checked}
+                onChange={() => filterGrades(key)}
+              />
+              <span className="text-lg px-3">{value}</span>
+            </label>
+          );
+        })}
+      </div>
     </div>
   );
 }
