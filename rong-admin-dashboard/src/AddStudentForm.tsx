cd4a9938399598da8a27745925120bbaa944b619
Austinling <94096647+Austinling@users.noreply.github.com>
2025-12-21 19:42:05 +0000

    Added blurred background and made grade into a dropdown

--- rong-admin-dashboard/src/AddStudentForm.tsx
+++ rong-admin-dashboard/src/AddStudentForm.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import { gradeMap } from "./GradeMap.ts";
 
 export function AddStudentForm() {
   const API_BASE = import.meta.env.VITE_API_BASE;
   const [name, setName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
-  const [grade, setGrade] = useState("");
+  const [grade, setGrade] = useState("一年级（上）");
 
   const handleSubmit = async () => {
     await fetch(`${API_BASE}/students`, {
@@ -25,29 +26,34 @@ export function AddStudentForm() {
   };
 
   return (
-    <div>
-      <label>Name</label>
-      <input
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      ></input>
+    <>
+      <div className="fixed inset-0 backdrop-blur-lg z-30 invisible"></div>
 
-      <label>Phone Number</label>
-      <input
-        placeholder="Phone Number"
-        value={phoneNumber}
-        onChange={(e) => setPhoneNumber(e.target.value)}
-      ></input>
-
-      <label>Grade</label>
-      <input
-        placeholder="Grade"
-        value={grade}
-        onChange={(e) => setGrade(e.target.value)}
-      ></input>
-
-      <button onClick={handleSubmit}>Add Student</button>
-    </div>
+      <div className="absolute border-2 bg-white z-40 w-100 h-100 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <label>Name</label>
+        <input
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <label>Phone Number</label>
+        <input
+          placeholder="Phone Number"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        ></input>
+        <label>Grade</label>
+        <select value={grade} onChange={(e) => setGrade(e.target.value)}>
+          {Object.entries(gradeMap).map(([key, value]) => {
+            return (
+              <option key={key} value={value} className="border-2">
+                {value}
+              </option>
+            );
+          })}
+        </select>
+        ;<button onClick={handleSubmit}>Add Student</button>
+      </div>
+    </>
   );
 }
