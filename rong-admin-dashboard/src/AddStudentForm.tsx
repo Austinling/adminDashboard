97ee8e371393d57c7352cdd3f854f7004d24c15e
Austinling <94096647+Austinling@users.noreply.github.com>
2025-12-22 02:15:00 +0000

    Made green and red popups if an addition of a new student is succesful or has empty fields

--- rong-admin-dashboard/src/AddStudentForm.tsx
+++ rong-admin-dashboard/src/AddStudentForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { gradeMap } from "./GradeMap.ts";
+import { PopUp } from "./PopUp.tsx";
 
 type StudentForm = {
   onClick: () => void;
@@ -11,13 +12,19 @@ export function AddStudentForm({ onClick, onSubmit }: StudentForm) {
   const [name, setName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [grade, setGrade] = useState("一年级（上）");
+  const [showPopUp, setPopUp] = useState(false);
+  const [showSuccessPopUp, setSuccessPopUp] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
     if (!name.trim() || !phoneNumber.trim() || !grade) {
-      alert("Please fill in all fields");
+      setPopUp(false);
+      setTimeout(() => setPopUp(true), 0);
       return;
+    } else if (name.trim() || phoneNumber.trim() || grade) {
+      setSuccessPopUp(false);
+      setTimeout(() => setSuccessPopUp(true), 0);
     }
 
     await fetch(`${API_BASE}/students`, {
@@ -41,6 +48,14 @@ export function AddStudentForm({ onClick, onSubmit }: StudentForm) {
   return (
     <>
       <div className="fixed inset-0 backdrop-blur-lg z-30"></div>
+
+      {showPopUp && (
+        <PopUp message="Please enter all fields" color="red" onOrOff={true} />
+      )}
+
+      {showSuccessPopUp && (
+        <PopUp message="User Added" color="green" onOrOff={true} />
+      )}
 
       <form className="absolute shadow-lg rounded-2xl flex flex-col bg-white z-40 w-100 h-100 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col gap-2 p-5">
